b4a7453f94fad88040a8f64235f84adb46bccbf1
Arthur <seu@email.comarthurcamilo112233@gmail.com>
2024-06-02 14:52:23 -0300

    Modificaçao

--- Aula_vivo.py
+++ Aula_vivo.py
@@ -13,3 +13,24 @@ for numero in range(maximo):
     numero = proximo
 
 print('Fora do for')
+
+valor_string = 'UltimaSchool'
+objeto_iter = iter(valor_string)
+
+while True:
+    try:
+        item = next(objeto_iter)
+        print(item)
+
+    except StopIteration:
+
+        break
+
+
+def fibonacci(maximo):
+    elemento_atual = 0 
+    proximo_elemento = 1 
+    while elemento_atual < maximo:
+        yield elemento_atual
+
+        elemento_atual, proximo_elemento = proximo_elemento, elemento_atual = proximo_elemento
--- Generators_Funcoes.py
+++ Generators_Funcoes.py
@@ -6,6 +6,13 @@ def fibonacci(maximo):
 
  
         elementoAtual, proximoElemento = proximoElemento, elementoAtual + proximoElemento
+'''     0                   0               0                   0                   0       = 0 
+        0                   1               1                   0                   1       = 1
+        1                   1               1                   1                   1       = 2
+        1                   2               2                   1                   2       = 3
+        2                   3               3                   2                   3       = 5
+        3                   5               5                   3                   5       = 8
+'''
 
 fib = fibonacci(1000000)
 for i in fib:
